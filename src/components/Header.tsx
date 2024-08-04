40dbffd9c1eb4da0e04e1f42b171171dfdf8bfe1
Dujo Perdic <dujo.perdic1@gmail.com>
2024-08-04 18:12:35 +0200

    removed balance from header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,47 +1,12 @@
-import { useConnection, useWallet } from "@solana/wallet-adapter-react";
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
-import { LAMPORTS_PER_SOL } from "@solana/web3.js";
-import { useEffect, useState } from "react";
 
 export default function Header() {
-  const [balance, setBalance] = useState(0);
-  const { connection } = useConnection();
-  const wallet = useWallet();
-
-  useEffect(() => {
-    if (!connection || !wallet?.publicKey) {
-      return;
-    }
-
-    connection.onAccountChange(
-      wallet.publicKey,
-      updatedAccountInfo => {
-        setBalance(updatedAccountInfo.lamports / LAMPORTS_PER_SOL);
-
-        // fetchTokenBalance();
-      },
-      { commitment: "confirmed" },
-    );
-
-    connection.getAccountInfo(wallet.publicKey).then(info => {
-      if (info) {
-        setBalance(info.lamports / LAMPORTS_PER_SOL);
-
-        // fetchTokenBalance();
-      }
-    });
-  }, [connection, wallet?.publicKey]);
-
   return (
     <header className="flex h-18 w-full bg-white shadow-sm">
       <nav className="flex h-full w-full sm:px-16 px-8 gap-4 items-center justify-between">
         <img src="/vite.svg" alt="vite" className="h-10" />
 
-        <span className="flex flex-row gap-4 items-center">
-          {wallet?.publicKey ? `Balance: ${balance} SOL` : ""}
-
-          <WalletMultiButton />
-        </span>
+        <WalletMultiButton />
       </nav>
     </header>
   );
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,43 +1,14 @@
-import { useConnection, useWallet } from "@solana/wallet-adapter-react";
-import { LAMPORTS_PER_SOL } from "@solana/web3.js";
-import { useEffect } from "react";
-import { useWalletStateStore } from "../state/store";
+import { useWallet } from "@solana/wallet-adapter-react";
 import Solana from "./Solana";
 import Token from "./Token";
 
 function Main() {
-  const { connection } = useConnection();
-  const wallet = useWallet();
-  const setWalletState = useWalletStateStore(state => state.setWalletState);
-
-  useEffect(() => {
-    if (!connection || !wallet?.publicKey) {
-      return;
-    }
-
-    connection.onAccountChange(
-      wallet.publicKey,
-      updatedAccountInfo => {
-        setWalletState({ solBalance: updatedAccountInfo.lamports / LAMPORTS_PER_SOL });
-
-        // fetchTokenBalance();
-      },
-      { commitment: "confirmed" },
-    );
-
-    connection.getAccountInfo(wallet.publicKey).then(info => {
-      if (info) {
-        setWalletState({ solBalance: info.lamports / LAMPORTS_PER_SOL });
-
-        // fetchTokenBalance();
-      }
-    });
-  }, [connection, wallet?.publicKey, setWalletState]);
+  const { publicKey } = useWallet();
 
   return (
     <main className="w-full flex-grow sm:px-16 px-8 py-8">
-      {wallet?.publicKey ? (
-        <div className="flex flex-col">
+      {publicKey ? (
+        <div className="flex flex-col gap-12">
           <Solana />
           <Token />
         </div>
