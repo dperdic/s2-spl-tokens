013216e0ddfc02d4201832e2648671119d579428
Dujo Perdic <dujo.perdic1@gmail.com>
2024-08-06 01:09:05 +0200

    create mint component

---
+++ src/components/CreateMint.tsx
@@ -0,0 +1,61 @@
+import {
+  getMinimumBalanceForRentExemptMint,
+  MINT_SIZE,
+  TOKEN_PROGRAM_ID,
+  createInitializeMintInstruction,
+} from "@solana/spl-token";
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import { Keypair, Transaction, SystemProgram } from "@solana/web3.js";
+import { TOKEN_DECIMALS } from "../utils/constants";
+import { useAddressStore } from "../state/store";
+import { confirmTransaction } from "../utils/functions";
+
+export default function CreateMint() {
+  const addresses = useAddressStore(state => state.addresses);
+  const setAddresses = useAddressStore(state => state.setAddresses);
+  const { publicKey, sendTransaction } = useWallet();
+  const { connection } = useConnection();
+
+  const createTokenMint = async () => {
+    if (!publicKey || !connection) {
+      return;
+    }
+
+    const lamports = await getMinimumBalanceForRentExemptMint(connection);
+
+    const mint = Keypair.generate();
+
+    const transaction = new Transaction().add(
+      SystemProgram.createAccount({
+        fromPubkey: publicKey,
+        newAccountPubkey: mint.publicKey,
+        space: MINT_SIZE,
+        lamports,
+        programId: TOKEN_PROGRAM_ID,
+      }),
+      createInitializeMintInstruction(mint.publicKey, TOKEN_DECIMALS, publicKey, publicKey, TOKEN_PROGRAM_ID),
+    );
+
+    const tx = await sendTransaction(transaction, connection, {
+      signers: [mint],
+    });
+
+    await confirmTransaction(connection, tx);
+
+    console.log(tx);
+
+    setAddresses({ ...addresses, mintAddress: mint.publicKey });
+  };
+
+  return (
+    <div className="flex flex-col gap-3">
+      <div>Mint address: {addresses.mintAddress?.toBase58()}</div>
+
+      <div>
+        <button type="button" className="btn btn-sm btn-blue" onClick={createTokenMint}>
+          Create mint
+        </button>
+      </div>
+    </div>
+  );
+}
