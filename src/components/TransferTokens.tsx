a0656efa00ed86434970ccaf4e45730c058ffd7e
Dujo Perdic <dujo.perdic1@gmail.com>
2024-08-08 12:45:21 +0200

    simplified function invocation

--- src/components/TransferTokens.tsx
+++ src/components/TransferTokens.tsx
@@ -117,13 +117,7 @@ export default function TransferTokens() {
         className="w-full sm:max-w-72 border px-3 py-2 shadow-sm block w-full border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm disabled:bg-gray-50 disabled:text-gray-500"
       />
 
-      <button
-        type="button"
-        className="btn btn-sm btn-blue"
-        onClick={async () => {
-          await transferTokens();
-        }}
-      >
+      <button type="button" className="btn btn-sm btn-blue" onClick={transferTokens}>
         Transfer tokens
       </button>
     </div>
