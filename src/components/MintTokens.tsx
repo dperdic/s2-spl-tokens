b95ce27ec466e08b411611739a445827aaf86307
Dujo Perdic <dujo.perdic1@gmail.com>
2024-08-09 12:16:34 +0200

    mint validation and disabled state

--- src/components/MintTokens.tsx
+++ src/components/MintTokens.tsx
@@ -12,6 +12,8 @@ import { confirmTransaction } from "../utils/functions";
 import { toast } from "react-toastify";
 
 export default function MintTokens() {
+  const transactionInProgress = useAppStore(state => state.transactionInProgress);
+  const setTransactionInProgress = useAppStore(state => state.setTransactionInProgress);
   const mint = useAppStore(state => state.mint);
   const tokenDecimals = useAppStore(state => state.tokenDecimals);
   const setTokenBalance = useAppStore(state => state.setTokenBalance);
@@ -20,18 +22,26 @@ export default function MintTokens() {
   const { publicKey, sendTransaction } = useWallet();
 
   const mintTokens = async () => {
+    setTransactionInProgress(true);
+
     if (!publicKey || !connection) {
       toast.error("Wallet not connected");
+      setTransactionInProgress(false);
+
       return;
     }
 
     if (!mint) {
       toast.error("No mint address provided, create a new mint");
+      setTransactionInProgress(false);
+
       return;
     }
 
     if (!mintAmount) {
       toast.error("Invalid mint amount");
+      setTransactionInProgress(false);
+
       return;
     }
 
@@ -41,6 +51,8 @@ export default function MintTokens() {
       mintAmountBigInt = BigInt(Number.parseFloat(mintAmount) * Math.pow(10, tokenDecimals));
     } catch (error) {
       toast.error("Invalid mint amount");
+      setTransactionInProgress(false);
+
       return;
     }
 
@@ -69,6 +81,8 @@ export default function MintTokens() {
 
       setTokenBalance(tokenAmount.value.uiAmount ?? 0);
     }
+    setMintAmount("");
+    setTransactionInProgress(false);
   };
 
   return (
@@ -78,13 +92,19 @@ export default function MintTokens() {
         placeholder="Amount"
         step={0.000000001}
         min={0}
+        value={mintAmount}
         onChange={event => {
           setMintAmount(event.target.value);
         }}
         className="w-full border px-3 py-2 shadow-sm block w-full border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm disabled:bg-gray-50 disabled:text-gray-500"
       />
 
-      <button type="button" className="btn btn-md btn-blue" onClick={mintTokens}>
+      <button
+        type="button"
+        className="btn btn-md btn-blue"
+        disabled={transactionInProgress || !mintAmount}
+        onClick={mintTokens}
+      >
         Mint tokens
       </button>
     </div>
