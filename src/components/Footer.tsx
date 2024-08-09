90d9aacfc83e8ab714c26e2a33723989001c032b
Dujo Perdic <dujo.perdic1@gmail.com>
2024-08-09 15:39:49 +0200

    left align

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="flex h-18 w-full sm:px-16 px-8 items-center bg-black text-white">
-      <div className="flex flex-grow h-full w-full py-4 justify-end">
+      <div className="flex flex-grow h-full w-full py-4">
         <a href="https://github.com/dperdic/s2-spl-tokens" target="_blank" className="flex flex-row gap-2">
           <img src="/github-mark-white.svg" alt="vite" className="h-5 inline-block" />
 
