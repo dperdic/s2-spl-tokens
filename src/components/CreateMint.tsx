da22ed769b3dce0b7a14f0c568b4f55d4f2255c6
Dujo Perdic <dujo.perdic1@gmail.com>
2024-08-09 16:20:49 +0200

    mint refactor

--- src/components/CreateMint.tsx
+++ src/components/CreateMint.tsx
@@ -12,6 +12,8 @@ import { toast } from "react-toastify";
 import { useState } from "react";
 
 export default function CreateMint() {
+  const transactionInProgress = useAppStore(state => state.transactionInProgress);
+  const setTransactionInProgress = useAppStore(state => state.setTransactionInProgress);
   const setMint = useAppStore(state => state.setMint);
   const setTokenDecimals = useAppStore(state => state.setTokenDecimals);
   const [localTokenDecimals, setLocalTokenDecimals] = useState<string>("");
@@ -19,13 +21,19 @@ export default function CreateMint() {
   const { connection } = useConnection();
 
   const createTokenMint = async () => {
+    setTransactionInProgress(true);
+
     if (!publicKey || !connection) {
       toast.error("Wallet not connected");
+      setTransactionInProgress(false);
+
       return;
     }
 
     if (localTokenDecimals === undefined) {
       toast.error("Number of token decimals not specified");
+      setTransactionInProgress(false);
+
       return;
     }
 
@@ -39,6 +47,8 @@ export default function CreateMint() {
       }
     } catch (error) {
       toast.error("Invalid number of decimals, valid range is 0-9");
+      setTransactionInProgress(false);
+
       return;
     }
 
@@ -77,6 +87,8 @@ export default function CreateMint() {
       setMint(mintKeypair.publicKey);
       setTokenDecimals(tokenDecimalsNumber);
     }
+
+    setTransactionInProgress(false);
   };
 
   return (
@@ -94,7 +106,12 @@ export default function CreateMint() {
           }}
           className="w-full border px-3 py-2 shadow-sm block w-full border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm disabled:bg-gray-50 disabled:text-gray-500"
         />
-        <button type="button" className="btn btn-md btn-blue" onClick={createTokenMint}>
+        <button
+          type="button"
+          className="btn btn-md btn-blue"
+          disabled={transactionInProgress}
+          onClick={createTokenMint}
+        >
           Create mint
         </button>
       </div>
