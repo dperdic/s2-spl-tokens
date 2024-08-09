cbc5949c675cc5cbd36fb9189deb7d53a0127522
Dujo Perdic <dujo.perdic1@gmail.com>
2024-08-09 10:16:03 +0200

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,10 @@
 export default function Footer() {
   return (
     <footer className="flex h-18 w-full sm:px-16 px-8 items-center text-sm bg-black text-white">
-      <div className="flex flex-grow h-full w-full">
-        <span>X</span>
-        &nbsp;
-        <span>by dperdic</span>
+      <div className="flex flex-grow h-full w-full py-4">
+        <a href="https://github.com/dperdic/s2-spl-tokens" target="_blank">
+          Source code
+        </a>
       </div>
     </footer>
   );
