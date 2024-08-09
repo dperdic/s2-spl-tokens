c79353c8564b4b63a5a7918cc4d11e294f88b1b0
Dujo Perdic <dujo.perdic1@gmail.com>
2024-08-09 11:19:41 +0200

    remove unnecessary div

--- src/components/Airdrop.tsx
+++ src/components/Airdrop.tsx
@@ -52,28 +52,26 @@ export default function Airdrop() {
 
   return (
     <div className="max-w-2xl w-full mx-auto">
-      <div className="w-full">
-        <h3 className="pb-4 max-w-2xl text-xl font-semibold">SOL</h3>
+      <h3 className="pb-4 max-w-2xl text-xl font-semibold">SOL</h3>
 
-        <div className="grid gap-4 p-4 bg-white rounded-md shadow w-full">
-          <div>Balance: {balance} SOL</div>
+      <div className="grid gap-4 p-4 bg-white rounded-md shadow w-full">
+        <div>Balance: {balance} SOL</div>
 
-          <div className="flex flex-col sm:flex-row gap-3">
-            <input
-              type="number"
-              placeholder="Amount"
-              step={0.000000001}
-              min={0}
-              onChange={event => {
-                setAirdropAmount(Number.parseFloat(event.target.value));
-              }}
-              className="w-full border px-3 py-2 shadow-sm block w-full border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm disabled:bg-gray-50 disabled:text-gray-500"
-            />
+        <div className="flex flex-col sm:flex-row gap-3">
+          <input
+            type="number"
+            placeholder="Amount"
+            step={0.000000001}
+            min={0}
+            onChange={event => {
+              setAirdropAmount(Number.parseFloat(event.target.value));
+            }}
+            className="w-full border px-3 py-2 shadow-sm block w-full border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm disabled:bg-gray-50 disabled:text-gray-500"
+          />
 
-            <button type="button" className="btn btn-md btn-blue" onClick={async () => await requestAirdrop()}>
-              Airdrop
-            </button>
-          </div>
+          <button type="button" className="btn btn-md btn-blue" onClick={async () => await requestAirdrop()}>
+            Airdrop
+          </button>
         </div>
       </div>
     </div>
