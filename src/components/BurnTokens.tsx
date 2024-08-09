d20bcd18cc8ee44916a9e86296b0a3db4affefa4
Dujo Perdic <dujo.perdic1@gmail.com>
2024-08-09 12:25:45 +0200

    burn disabled state and form reset

--- src/components/BurnTokens.tsx
+++ src/components/BurnTokens.tsx
@@ -7,6 +7,8 @@ import { confirmTransaction } from "../utils/functions";
 import { toast } from "react-toastify";
 
 export default function BurnTokens() {
+  const transactionInProgress = useAppStore(state => state.transactionInProgress);
+  const setTransactionInProgress = useAppStore(state => state.setTransactionInProgress);
   const mint = useAppStore(state => state.mint);
   const tokenBalance = useAppStore(state => state.tokenBalance);
   const setTokenBalance = useAppStore(state => state.setTokenBalance);
@@ -16,18 +18,26 @@ export default function BurnTokens() {
   const { publicKey, sendTransaction } = useWallet();
 
   const burnTokens = async () => {
+    setTransactionInProgress(true);
+
     if (!publicKey || !connection) {
       toast.error("Wallet not connected");
+      setTransactionInProgress(false);
+
       return;
     }
 
     if (!mint) {
       toast.error("No mint address provided, create a new mint");
+      setTransactionInProgress(false);
+
       return;
     }
 
     if (!burnAmount) {
       toast.error("Invalid burn amount");
+      setTransactionInProgress(false);
+
       return;
     }
 
@@ -37,6 +47,8 @@ export default function BurnTokens() {
       burnAmountBigInt = BigInt(Number.parseFloat(burnAmount) * Math.pow(10, tokenDecimals));
     } catch (error) {
       toast.error("Invalid burn amount");
+      setTransactionInProgress(false);
+
       return;
     }
 
@@ -46,6 +58,8 @@ export default function BurnTokens() {
       await getAccount(connection, ata);
     } catch (error) {
       toast.error("You don't have an associated token account for this token, mint a token to create one");
+      setTransactionInProgress(false);
+
       return;
     }
 
@@ -66,6 +80,9 @@ export default function BurnTokens() {
 
       setTokenBalance(tokenAmount.value.uiAmount ?? 0);
     }
+
+    setBurnAmount("");
+    setTransactionInProgress(false);
   };
 
   return (
@@ -77,13 +94,19 @@ export default function BurnTokens() {
           step={0.000000001}
           min={0}
           max={tokenBalance}
+          value={burnAmount}
           onChange={event => {
             setBurnAmount(event.target.value);
           }}
           className="w-full border px-3 py-2 shadow-sm block w-full border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm disabled:bg-gray-50 disabled:text-gray-500"
         />
 
-        <button type="button" className="btn btn-md btn-blue" disabled={!tokenBalance} onClick={burnTokens}>
+        <button
+          type="button"
+          className="btn btn-md btn-blue"
+          disabled={transactionInProgress || !tokenBalance || !burnAmount}
+          onClick={burnTokens}
+        >
           Burn tokens
         </button>
       </div>
